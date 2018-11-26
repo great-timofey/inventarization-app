55ca884b61380944d39b05d4721d0c32f0592ecb
Sibirtsev Aleksandr <asibirtsev@allmax.team>
2018-11-26 17:02:56 +0700

    Add gradient button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,25 @@
+// @flow
+
+import React from 'react';
+import { Text, TouchableWithoutFeedback } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import styles from './styles';
+import colors from '../../global/colors';
+
+import type { ButtonProps } from './types';
+
+const Button = ({ title, onPress }: ButtonProps) => (
+  <TouchableWithoutFeedback onPress={onPress}>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      colors={[colors.green, colors.darkGreen]}
+      style={styles.linearGradient}
+    >
+      <Text style={styles.buttonText}>{title}</Text>
+    </LinearGradient>
+  </TouchableWithoutFeedback>
+);
+
+export default Button;
---
+++ src/global/colors.js
@@ -0,0 +1,5 @@
+export default {
+  white: '#FFFFFF',
+  green: '#39FA90',
+  darkGreen: '#19D191',
+};
