bbd9866ebf895a8699a0b4e988ef9dbc675eea70
Sibirtsev Aleksandr <asibirtsev@allmax.team>
2018-11-28 09:46:07 +0700

    Add SwipeListItem component

--- src/global/colors.js
+++ src/global/colors.js
@@ -1,5 +1,14 @@
 export default {
   white: '#FFFFFF',
+  black: '#000000',
   green: '#39FA90',
   darkGreen: '#19D191',
+  blue: '#5AC8FA',
+  red: '#FF2D55',
+  gray: '#F6F6F6',
+  text: {
+    gray: '#BDBDBD',
+    blue: '#42BCF8',
+  },
+  border: '#E4E4E4',
 };
---
+++ src/global/images.js
@@ -0,0 +1,9 @@
+// @flow
+
+import editIcon from '../assets/images/editIcon.png';
+import deleteIcon from '../assets/images/deleteIcon.png';
+
+export default {
+  editIcon,
+  deleteIcon,
+};
