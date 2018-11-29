08bf21db2904c0701f6bcd5c727cc59d8b6194d0
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-11-29 10:38:42 +0700

    added apollo client and apollo provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 //  @flow
 import React from 'react';
 import AppNavigator from 'navigation';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'https://api.staging.inventoryapp.info/graphql',
+});
 
 type Props = {};
 
-const App = (): Props => <AppNavigator isAuth />;
+const App = (): Props => (
+  <ApolloProvider client={client}>
+    <AppNavigator isAuth />
+  </ApolloProvider>
+);
 
 export default App;
