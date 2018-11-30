35e60f465e037b52220e94d2ad903f4731ce415f
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-11-30 14:10:47 +0700

    added react-native-vector-icons + tested android

--- src/scenes/Items/index.js
+++ src/scenes/Items/index.js
@@ -2,12 +2,19 @@
 import React, { PureComponent } from 'react';
 import { Text, View } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import styles from './styles';
 
 type Props = {};
 class ItemsScene extends PureComponent<Props> {
   static navigationOptions = () => ({
     title: 'Предметы',
+    headerLeft: () => (
+      <Icon.Button name="facebook" backgroundColor="#3b5998">
+        Login with Facebook
+      </Icon.Button>
+    ),
   });
 
   render() {
