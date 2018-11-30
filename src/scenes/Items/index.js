32474913df7dd83449cd05090bdd4090b87d4f45
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-11-30 17:58:35 +0700

    improved header icons styles for items scene

--- src/scenes/Items/index.js
+++ src/scenes/Items/index.js
@@ -3,31 +3,54 @@ import React, { PureComponent } from 'react';
 import { Text, View } from 'react-native';
 
 import InventoryIcon from 'assets/InventoryIcon';
+import Icon from 'react-native-vector-icons/Feather';
 
 import colors from 'global/colors';
+import globalStyles from 'global/styles';
 import styles from './styles';
 
 type Props = {};
+const iconProps = {
+  size: 25,
+  borderRadius: 0,
+  iconStyle: globalStyles.iconStyle,
+  backgroundColor: colors.transparent,
+};
 
 class ItemsScene extends PureComponent<Props> {
   static navigationOptions = () => ({
-    title: 'Предметы',
-
-    headerLeft: (
-      <InventoryIcon.Button
-        size={25}
-        name="tile"
-        backgroundColor="transparent"
-        color={colors.accent}
-      />
-    ),
-    headerRight: (
-      <InventoryIcon.Button
-        size={25}
-        name="search"
-        backgroundColor="transparent"
-        color={colors.accent}
-      />
+    header: () => (
+      <View
+        style={{
+          height: 65,
+          paddingTop: 20,
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: 'white',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Icon.Button
+          {...iconProps}
+          name="grid"
+          backgroundColor="transparent"
+          color={colors.accent}
+        />
+        <View style={{ flexDirection: 'row', paddingRight: 10 }}>
+          <InventoryIcon.Button
+            {...iconProps}
+            name="dashboard"
+            backgroundColor="transparent"
+            color={colors.accent}
+          />
+          <InventoryIcon.Button
+            {...iconProps}
+            name="search"
+            backgroundColor="transparent"
+            color={colors.accent}
+          />
+        </View>
+      </View>
     ),
   });
 
