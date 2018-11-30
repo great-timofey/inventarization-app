4b42dc6806c451b5edb03e100456a0c233d71fdc
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-11-30 10:22:30 +0700

    removed ratebutton

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,38 +1,85 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { PureComponent } from 'react';
+import { Text, View, Alert } from 'react-native';
 
-import RateButton from 'components/RateButton';
+import { compose, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import Button from 'components/Button';
+
+const ADD_STAR_MUTATION = gql`
+  mutation addStar($repoId: ID!) {
+    addStar(input: { starrableId: $repoId }) {
+      starrable {
+        id
+      }
+    }
+  }
+`;
+
+const REMOVE_STAR_MUTATION = gql`
+  mutation removeStar($repoId: ID!) {
+    removeStar(input: { starrableId: $repoId }) {
+      starrable {
+        id
+      }
+    }
+  }
+`;
 
 type Props = {
   name: string,
   description: string,
   languageName: string,
   repoId: string,
+  addStar: () => void,
+  removeStar: () => void,
 };
 
-function Item({ name, description, languageName, repoId }: Props) {
-  return (
-    <View
-      style={{
-        marginBottom: 5,
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
-      }}
-    >
-      <Text>
-        {name} : {description} : {languageName}
-      </Text>
+class Item extends PureComponent<Props> {
+  addStar = async () => {
+    const { repoId, addStar } = this.props;
+    await addStar({ variables: { repoId } });
+    Alert.alert('Star has been successfully added');
+  };
+
+  removeStar = async () => {
+    const { repoId, removeStar } = this.props;
+    await removeStar({ variables: { repoId } });
+    Alert.alert('Star has been successfully removed');
+  };
+
+  render() {
+    const { name, description, languageName } = this.props;
+    return (
       <View
         style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          marginBottom: 5,
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
         }}
       >
-        <RateButton repoId={repoId} forAdding />
-        <RateButton repoId={repoId} forAdding={false} />
+        <Text>
+          {name} : {description} : {languageName}
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Button title="Add Star" onPress={this.addStar} />
+          <Button title="Remove Star" onPress={this.removeStar} />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
-export default Item;
+export default compose(
+  graphql(ADD_STAR_MUTATION, {
+    name: 'addStar',
+  }),
+  graphql(REMOVE_STAR_MUTATION, {
+    name: 'removeStar',
+  })
+)(Item);
--- src/components/RateButton/types.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-export type ButtonProps = {
-  title: string,
-  onPress?: () => void,
-  forAdding?: boolean,
-  repoId: number,
-};
