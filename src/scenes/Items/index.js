07ec49735ca103206a6b15a06ad30b82c40d6ef6
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-11-30 12:48:28 +0700

    added mutation

--- src/scenes/Items/index.js
+++ src/scenes/Items/index.js
@@ -2,13 +2,14 @@
 import React, { PureComponent } from 'react';
 import {
   Text,
+  View,
+  Button,
   FlatList,
   TouchableOpacity,
-  View,
   ActivityIndicator,
 } from 'react-native';
 
-import { withApollo } from 'react-apollo';
+import { withApollo, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Item from 'components/Item';
@@ -36,13 +37,32 @@ const GET_REPOS_QUERY = gql`
   }
 `;
 
-type Props = {};
-class ItemsScene extends PureComponent<Props> {
+const SIGN_IN_MUTATION = gql`
+  mutation SignInUser(input: {$email: String!, $password:String!}) {
+    signInUser(input: {email: $email, password: $password}) {
+      message
+      token
+    }
+  }
+`;
+
+type Props = { client: Object };
+type State = { data: ?Object, loading: boolean };
+class ItemsScene extends PureComponent<Props, State> {
   state = {
     data: null,
     loading: false,
   };
 
+  signIn = () => {
+    const user = {
+      email: 'ttverdokhlebov@some.team',
+      password: '11112222',
+    };
+    return <Mutation mutation={SIGN_IN_MUTATION}>{}</Mutation>;
+  };
+
+  /*
   getItems = async () => {
     this.setState({ loading: true });
     const { client } = this.props;
@@ -50,7 +70,6 @@ class ItemsScene extends PureComponent<Props> {
       query: GET_REPOS_QUERY,
       variables: { login: 'great-timofey' },
     });
-    console.log(data);
     this.setState({ data, loading: false });
   };
 
@@ -65,35 +84,43 @@ class ItemsScene extends PureComponent<Props> {
     },
   }) => (
     <Item
+      repoId={id}
       name={name}
       description={description}
-      repoId={id}
       languageName={languageName}
     />
   );
+  */
 
   render() {
     const { loading, data } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.instructions}>ITEMS</Text>
-        {loading && <ActivityIndicator />}
-        {data ? (
-          <FlatList
-            keyExtractor={item => item.node.id}
-            data={data.user.repositories.edges}
-            renderItem={this.renderItem}
-          />
-        ) : (
-          !loading && (
-            <TouchableOpacity onPress={this.getItems}>
-              <Text>Get Info from Github API</Text>
-            </TouchableOpacity>
-          )
-        )}
+        <Button
+          onPress={this.signIn}
+          style={{ backgroundColor: 'green', flex: 1 }}
+          title="sign in"
+        />
       </View>
     );
   }
 }
+/*
+  {loading && <ActivityIndicator />}
+  {data ? (
+    <FlatList
+      keyExtractor={item => item.node.id}
+      data={data.user.repositories.edges}
+      renderItem={this.renderItem}
+    />
+  ) : (
+    !loading && (
+      <TouchableOpacity onPress={this.getItems}>
+        <Text>Get Info from Github API</Text>
+      </TouchableOpacity>
+    )
+  )}
+*/
 
 export default withApollo(ItemsScene);
