772d27bcca35baf6090913ae020fc3f385446660
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-12-03 10:47:15 +0700

    added utils helpers

---
+++ src/global/utils.js
@@ -0,0 +1,12 @@
+import { Platform } from 'react-native';
+
+export const getPlaceholder = size =>
+  `https://via.placeholder.com/${size}x${size}`;
+
+export const platformSelect = (ios, android) =>
+  Platform.select({ ios, android });
+
+export default {
+  getPlaceholder,
+  platformSelect,
+};
