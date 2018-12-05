0b733db5011cbccca8ad59c65d2b48ed04fb3cbd
Sibirtsev Aleksandr <asibirtsev@allmax.team>
2018-12-05 19:17:18 +0700

    Add HeaderBackButton

---
+++ src/components/HeaderBackButton/styles.js
@@ -0,0 +1,11 @@
+// @flow
+
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  headerLeft: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    alignSelf: 'flex-start',
+  },
+});
