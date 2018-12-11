96fefdde51a2a577e4915c531eab4d560be59e88
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-12-11 18:38:29 +0700

    Fixed size normalization for add button

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -4,19 +4,22 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import { normalize } from 'global/utils';
 import colors from 'global/colors';
 import type { Props } from './types';
 
 const AddButton = (props: Props) => (
   <Icon.Button
     {...props}
-    size={34}
+    size={normalize(34)}
     name="ios-add-circle"
     color={colors.buttonGreen}
     activeOpacity={0.5}
     underlayColor={colors.transparent}
     backgroundColor={colors.transparent}
-    iconStyle={{ marginRight: 0 }}
+    iconStyle={{
+      marginRight: 0,
+    }}
   />
 );
 
