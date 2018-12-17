b3e54fca6e4ee913b209c07684cf11cd63df2785
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-12-17 12:27:27 +0700

    Fixed auth

--- src/scenes/Auth/Login/index.js
+++ src/scenes/Auth/Login/index.js
@@ -166,7 +166,7 @@ class Login extends PureComponent<Props, State> {
       navigation,
       signInMutation,
       signUpMutation,
-      // setAuthMutationClient,
+      setAuthMutationClient,
     } = this.props;
 
     /**
