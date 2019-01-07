3586dcef647b6de4e8fd5806ac399e3464cdbb67
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2019-01-07 08:13:54 +0700

    add updateAsset mutation

--- src/graphql/assets/mutations.js
+++ src/graphql/assets/mutations.js
@@ -82,6 +82,59 @@ export const CREATE_ASSET = gql`
   }
 `;
 
+export const UPDATE_ASSET = gql`
+  mutation UpdateAsset(
+    $id: ID!
+    $name: String!
+    $description: String
+    $purchasePrice: Float
+    $dateOfPurchase: String
+    $assessedValue: Float
+    $assessedDate: String
+    $guaranteeExpires: String
+    $gps: GpsAttr!
+    $inventoryId: String!
+    $manufacture: String
+    $model: String
+    $quantity: Int
+    $categoryId: ID
+    $placeId: ID
+    $photos: [Upload!]
+    $photosOfDamages: [Upload!]
+    $responsibleId: ID
+    $status: AssetStatus
+    $onTheBalanceSheet: Boolean
+  ) {
+    updateAsset(
+      id: $id
+      attributes: {
+        name: $name
+        description: $description
+        purchasePrice: $purchasePrice
+        dateOfPurchase: $dateOfPurchase
+        assessedValue: $assessedValue
+        assessedDate: $assessedDate
+        guaranteeExpires: $guaranteeExpires
+        gps: $gps
+        inventoryId: $inventoryId
+        manufacture: $manufacture
+        model: $model
+        quantity: $quantity
+        categoryId: $categoryId
+        placeId: $placeId
+        photos: $photos
+        photosOfDamages: $photosOfDamages
+        responsibleId: $responsibleId
+        status: $status
+        onTheBalanceSheet: $onTheBalanceSheet
+      }
+    ) {
+      id
+    }
+  }
+`;
+
 export default {
   CREATE_ASSET,
+  UPDATE_ASSET,
 };
