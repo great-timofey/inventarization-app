ac9dd63ce998b44e9862907f197dc846da45dc1c
Dmitriy Shishkin <dshishkin@allmax.team>
2019-01-09 10:49:48 +0700

    Adjust eslint rules

--- src/scenes/AddItem/AddItemPhotos/index.js
+++ src/scenes/AddItem/AddItemPhotos/index.js
@@ -115,7 +115,9 @@ class AddItemPhotos extends PureComponent<Props, State> {
     const { isHintOpened, needToAskPermissions } = this.state;
     this.setState({ isLoading: true });
 
-    if (needToAskPermissions) await this.askPermissions();
+    if (needToAskPermissions) {
+      await this.askPermissions();
+    }
 
     const { ableToTakePicture } = this.state;
 
@@ -123,7 +125,9 @@ class AddItemPhotos extends PureComponent<Props, State> {
       const options = { quality: 0.5, base64: true };
       const { base64, uri } = await this.camera.takePictureAsync(options);
 
-      if (isHintOpened) this.setState({ isHintOpened: false });
+      if (isHintOpened) {
+        this.setState({ isHintOpened: false });
+      }
 
       let location = new Promise((res, rej) => {
         navigator.geolocation.getCurrentPosition(
