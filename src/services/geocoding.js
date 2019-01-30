58a515108321c3d88f2658ec49230abbc2914337
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2019-01-30 16:29:14 +0700

    Add language=ru + response type street

--- src/services/geocoding.js
+++ src/services/geocoding.js
@@ -7,7 +7,7 @@ const ANDROID_KEY = 'something';
 const API_KEY = isIOS ? IOS_KEY : ANDROID_KEY;
 
 export const getAddressByCoords = (lat, lon) =>
-  new Promise((res, rej) => fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${COMMON_KEY}`)
+  new Promise((res, rej) => fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&language=ru&result_type=street_address&key=${COMMON_KEY}`)
     .then(response => res(response))
     .catch(err => rej(err)));
 
